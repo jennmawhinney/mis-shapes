31df1ab575f9937ecf038958f78c43178f2af16d
jennmawhinney <jennnobile@icloud.com>
2016-05-09 21:40:56 +1000

    sounds working and basic layout sorted

--- app/assets/javascripts/_global-var.js
+++
@@ -1,7 +0,0 @@
-// var winWidth = window.innerWidth;
-// var winHeight = window.innerHeight;
-//
-// var $animWindow = 0;
-// var $loopWindow = 0;
-//
-// var $loopHead = 0;
--- app/assets/javascripts/sounds.js
+++ app/assets/javascripts/sounds.js
@@ -1,146 +1,131 @@
-$(document).ready(function() {
-
-//   // var count;
-// $('.container').on('click', function(){
-//   movePlayhead();
+// $(document).ready(function() {
+//
+//   var loopArray = [];
+//   window.AudioContext = window.AudioContext || window.webkitAudioContext;
+//   context = new AudioContext();
+//
+// var audioCtx = new AudioContext();
+// console.log(audioCtx.currentTime);
+//
+// $(document).on("click", function() {
+//   console.log(audioCtx.currentTime);
 // });
 //
-// var movePlayhead = function(){
-//   var clock = new WAAClock(audioContext);
-//   clock.start();
-//   debugger;
-//   var startTime = context.currentTime;
-//   var tempo = 120; // BPM (beats per minute)
-//   var quarterNoteTime = 60 / tempo;
-//   $('playhead').animate({
+// var noise = function(div, name, length) {
+//     // mediaRecorder.start(0);
+//   // $(div).removeClass("clicked").addClass("clicked");
+//   var sound = new Howl({
+//     urls: ['/assets/' + name + '.mp3'],
+//     sprite: {
+//       [name]: [0, length]
+//     },
+//     loop: false
+//   });
+//   //   if($(div).hasClass("clicked")){
+//   //     $(div).removeClass("clicked");
+//   //   } else {
+//   //   $(div).addClass("clicked");
+//   // }
+//   //   if ($(div).hasClass('clicked', function(){
+//   loopArray.push([name]);
+//   setInterval(function(time) {
+//     for (var i = 0; i < loopArray.length; i++) {
+//         sound.play(loopArray[i]);
+//       }
+//   } , 4350);
+// // }));
 //
-// });
+// var mediaConstraints = {
+// audio: true
 // };
-//   var metronome = function(count){
-//     // debugger;
-//     count = 1;
-//     for (var i = 0; i < count.length; i++) {
+// };
 //
-//       count++;
-//     }
-//     if(count === 1){
-//       $('.count').removeClass('blink');
-//       $('#1').addClass('blink');
-//   } else if(count === 2){
-//       $('.count').removeClass('blink');
-//       $('#2').addClass('blink');
-//     } else if(count === 3){
-//       $('.count').removeClass('blink');
-//       $('#3').addClass('blink');
-//     } else if(count === 4){
-//       $('.count').removeClass('blink');
-//       $('#4').addClass('blink');
-//       count = 1;
-//     }
-//     };
-//   metronome(4);
-
-    var noise = function(div, name, length) {
-      // debugger;
-      // mediaRecorder.start(0);
-
-      $(div).removeClass("clicked").addClass("clicked");
-      var sound = new Howl({
-        urls: ['assets/' + name + '.m4a'],
-        sprite: {
-          [name]: [0, length]
-        },
-        loop: true
-      });
-      sound.play(name);
-    };
-
-    var mediaConstraints = {
-    audio: true
-};
-
-  $('#a').on('click', function(event) {
-    event.preventDefault();
-    noise('#a', 'conga', 4000);
-  });
-
-  $('#b').on('click', function(event) {
-    event.preventDefault();
-    noise('#b', 'ambition_beat', 8000);
-  });
-
-  $('#c').on('click', function(event) {
-    event.preventDefault();
-    noise('#c', 'big_maracas', 4000);
-  });
-
-  $('#d').on('click', function(event) {
-    event.preventDefault();
-    noise('#d', 'conga_flam', 4000);
-  });
-
-  $('#e').on('click', function(event) {
-    event.preventDefault();
-    noise('#e', 'conga_house', 4000);
-  });
-
-  // $('#f').on('click', function() {
-  //   noise('#f', 'arpeggio_bass', 8000);
-  // });
-
-  // $('#g').on('click', function() {
-  //   noise('#g', 'bass', 4000);
-  // });
-
-  $('#h').on('click', function(event) {
-    event.preventDefault();
-    noise('#h', 'cool_bass', 4000);
-  });
-
-  $('#i').on('click', function(event) {
-    event.preventDefault();
-    noise('#i', 'dance_floor', 4000);
-  });
-
-  $('#j').on('click', function(event) {
-    event.preventDefault();
-    noise('#j', 'fat_bass', 8000);
-  });
-
-  $('#k').on('click', function(event) {
-    event.preventDefault();
-    noise('#k', 'wilder_bass', 8000);
-  });
-
-  $('#l').on('click', function(event) {
-    event.preventDefault();
-    noise('#l', 'bailey_background', 4000);
-  });
-
-  $('#m').on('click', function(event) {
-    event.preventDefault();
-    noise('#m', 'chord_foundation', 4000);
-  });
-
-  $('#n').on('click', function(event) {
-    event.preventDefault();
-    noise('#n', 'piano', 4000);
-  });
-
-  $('#o').on('click', function(event) {
-    event.preventDefault();
-    noise('#m', 'rainy_day', 3900);
-  });
-});//closes document.ready
-
-  // $('#e').on('click', function() {
-  //   noise('conga_house', 4000);
-  // });
-  //
-  // $('#e').on('click', function() {
-  //   noise('conga_house', 4000);
-  // });
-  //
-  // $('#e').on('click', function() {
-  //   noise('conga_house', 4000);
-  // });
+//
+// $('#a').on('click', function(event) {
+//   event.preventDefault();
+//   noise('#a', 'drum.1.1', 4350);
+// });
+//
+// $('#b').on('click', function(event) {
+//   event.preventDefault();
+//   noise('#b', 'drum.1.2', 4350);
+// });
+//
+// $('#c').on('click', function(event) {
+//   event.preventDefault();
+//   noise('#c', 'drum.1.3', 4350);
+// });
+//
+// $('#d').on('click', function(event) {
+//   event.preventDefault();
+//   noise('#d', 'drum.1.4', 4350);
+// });
+//
+// $('#e').on('click', function(event) {
+//   event.preventDefault();
+//   noise('#e', 'sounds.1.1', 4350);
+// });
+//
+// // $('#f').on('click', function() {
+// //   noise('#f', 'arpeggio_bass', 4350);
+// // });
+//
+// // $('#g').on('click', function() {
+// //   noise('#g', 'bass', 4000);
+// // });
+//
+// $('#h').on('click', function(event) {
+//   event.preventDefault();
+//   noise('#h', 'sounds.1.2', 4350);
+// });
+//
+// $('#i').on('click', function(event) {
+//   event.preventDefault();
+//   noise('#i', 'sounds.1.3', 4350);
+// });
+//
+// $('#j').on('click', function(event) {
+//   event.preventDefault();
+//   noise('#j', 'sounds.1.4', 4350);
+// });
+//
+// $('#k').on('click', function(event) {
+//   event.preventDefault();
+//   noise('#k', 'bass.1.1', 4350);
+// });
+//
+// $('#l').on('click', function(event) {
+//   event.preventDefault();
+//   noise('#l', 'bass.1.2', 4350);
+// });
+//
+// $('#m').on('click', function(event) {
+//   event.preventDefault();
+//   noise('#m', 'bass.1.3', 4350);
+// });
+//
+// $('#n').on('click', function(event) {
+//   event.preventDefault();
+//   noise('#n', 'bass.1.4', 4350);
+// });
+//
+// $('#o').on('click', function(event) {
+//   event.preventDefault();
+//   noise('#m', 'sounds.1.5', 4350);
+// });
+//
+// // init();
+// // });//closes document.ready
+//
+// $('#e').on('click', function() {
+//   noise('conga_house', 4000);
+// });
+//
+// $('#e').on('click', function() {
+//   noise('conga_house', 4000);
+// });
+//
+// $('#e').on('click', function() {
+//   noise('conga_house', 4000);
+// });
+// });
